880cc26428896ab587c767f4136b2147db040df0
devlights <gsf.zero1@devlights.onmicrosoft.com>
2018-10-29 12:58:14 +0900

    コメント更新

--- basic/helloworld/main.go
+++ basic/helloworld/main.go
@@ -4,5 +4,11 @@ package main
 import "fmt"
 
 func main() {
+	// Golang には、 ビルドイン関数の println と
+	// fmt.Println という　名前の似ている２つの関数があるが
+	// 基本的に、どのサンプルも fmt.Println を利用している。
+	//
+	// 理由は、Golang のドキュメントに以下のように記載されているから。
+	//   https://golang.org/pkg/builtin/#println
 	fmt.Println("Hello World!")
 }
