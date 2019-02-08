2f9948217a762dd0a135b0064450c74f767d53d4
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-02-08 13:21:59 +0900

    add reflect.TypeOf() の結果を表示するサンプル

---
+++ advanced/reflection01/main.go
@@ -0,0 +1,24 @@
+// reflect パッケージのサンプルです。
+//
+// 型オブジェクトの取得について。
+// 参考情報：http://bit.ly/2UON9BD
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// reflect.TypeOf() のサンプルです。
+func main() {
+	// 特定の値の型を調べる場合には、reflect.TypeOf() を利用する
+	// C# でいう、 "hello world".GetType() と同じような感じ
+	// Name メソッドで名前を取得できる
+	i := 0
+	t1 := reflect.TypeOf(i)
+	fmt.Printf("%s\n", t1.Name())
+
+	s := "hello world"
+	t2 := reflect.TypeOf(s)
+	fmt.Printf("%s\n", t2.Name())
+}
