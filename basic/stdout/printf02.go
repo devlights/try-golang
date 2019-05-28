843e589bf4412fd01fa8d70b95a6dfe92ef233de
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-05-28 11:08:14 +0900

    Update stdout/printf02 example
    - %#v と %T の書式サンプルを追加

--- basic/stdout/printf02.go
+++ basic/stdout/printf02.go
@@ -17,20 +17,32 @@ func Printf02() error {
 	//     https://golang.org/pkg/fmt/#hdr-Printing
 	// ------------------------------------------------------
 	// %s は文字列
-	messageFormat := "Hello %s\n"
+	messageFormat := "[%%s] Hello %s\n"
 	fmt.Printf(messageFormat, "World")
 
 	// %d は数値
-	messageFormat = "Hello %d\n"
+	messageFormat = "[%%d] Hello %d\n"
 	fmt.Printf(messageFormat, 100)
 
 	// %v は デフォルトのフォーマットを用いて値を表示
-	messageFormat = "Hello %v\n"
+	messageFormat = "[%%v] Hello %v\n"
 	fmt.Printf(messageFormat, MyType{i: 100, v: 111, x: 222, y: 333})
 
-	// %+v は %v に加えて構造体の場合にフィールド名も出力してくれる
-	messageFormat = "Hello %+v\n"
+	// %#v は、Goのリテラル表現で値を出力
+	messageFormat = "[%%#v] Hello %#v\n"
 	fmt.Printf(messageFormat, MyType{i: 100, v: 111, x: 222, y: 333})
+
+	// %+v は %v に加えて構造体の場合にフィールド名も出力
+	messageFormat = "[%%+v] Hello %+v\n"
+	fmt.Printf(messageFormat, MyType{i: 100, v: 111, x: 222, y: 333})
+
+	// %T は データの型情報を出力
+	data := MyType{i: 100, v: 111, x: 222, y: 333}
+	intArray := [...]int{1, 2, 3}
+	intSlice := []int{1, 2, 3}
+	intKeyMap := map[int]string{1: "apple"}
+	messageFormat = "[%%T] Hello %T, %T, %T, %T, %T, %T\n"
+	fmt.Printf(messageFormat, data, "hello", 100, intArray, intSlice, intKeyMap)
 
 	return nil
 }
