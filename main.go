ffa4da149ad32b5d6c01af8fec0b6ebe5500e536
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-06-05 14:05:20 +0900

    Update main.go
    - Add init()
    - Remove makeMapping()

--- main.go
+++ main.go
@@ -24,14 +24,45 @@ import (
 	"strings"
 )
 
+// サンプル関数のマッピング
+var mapping = make(map[string]func() error)
+
+// 初期化関数
+func init() {
+	mapping["helloworld"] = helloworld.HelloWorld
+	mapping["printf01"] = stdout.Printf01
+	mapping["printf02"] = stdout.Printf02
+	mapping["printf03"] = stdout.Printf03
+	mapping["println01"] = stdout.Println01
+	mapping["scanner01"] = stdin.Scanner01
+	mapping["map01"] = map_.Map01
+	mapping["scope01"] = scope.Scope01
+	mapping["async01"] = async.Async01
+	mapping["reflection01"] = reflection.Reflection01
+	mapping["import01"] = import_.Import01
+	mapping["fileio01"] = io_.FileIo01
+	mapping["fileio02"] = io_.FileIo02
+	mapping["fileio03"] = io_.FileIo03
+	mapping["fileio04"] = io_.FileIo04
+	mapping["interface01"] = interface_.Interface01
+	mapping["struct01"] = struct_.Struct01
+	mapping["array01"] = array_.Array01
+	mapping["slice01"] = slice_.Slice01
+	mapping["slice02"] = slice_.Slice02
+	mapping["slice03"] = slice_.Slice03
+	mapping["slice04"] = slice_.Slice04
+	mapping["slice05"] = slice_.Slice05
+	mapping["comment01"] = comments.Comment01
+	mapping["closure01"] = closure.Closure01
+}
+
+// メインエントリポイント
 func main() {
 	var (
 		onetime = flag.Bool("onetime", false, "run only one time")
 	)
 
 	flag.Parse()
-
-	mapping := makeMappings()
 
 	fmt.Print("ENTER EXAMPLE NAME: ")
 
@@ -61,36 +92,4 @@ func main() {
 	}
 
 	fmt.Println("END")
-}
-
-func makeMappings() map[string]func() error {
-	mapping := make(map[string]func() error)
-
-	mapping["helloworld"] = helloworld.HelloWorld
-	mapping["printf01"] = stdout.Printf01
-	mapping["printf02"] = stdout.Printf02
-	mapping["printf03"] = stdout.Printf03
-	mapping["println01"] = stdout.Println01
-	mapping["scanner01"] = stdin.Scanner01
-	mapping["map01"] = map_.Map01
-	mapping["scope01"] = scope.Scope01
-	mapping["async01"] = async.Async01
-	mapping["reflection01"] = reflection.Reflection01
-	mapping["import01"] = import_.Import01
-	mapping["fileio01"] = io_.FileIo01
-	mapping["fileio02"] = io_.FileIo02
-	mapping["fileio03"] = io_.FileIo03
-	mapping["fileio04"] = io_.FileIo04
-	mapping["interface01"] = interface_.Interface01
-	mapping["struct01"] = struct_.Struct01
-	mapping["array01"] = array_.Array01
-	mapping["slice01"] = slice_.Slice01
-	mapping["slice02"] = slice_.Slice02
-	mapping["slice03"] = slice_.Slice03
-	mapping["slice04"] = slice_.Slice04
-	mapping["slice05"] = slice_.Slice05
-	mapping["comment01"] = comments.Comment01
-	mapping["closure01"] = closure.Closure01
-
-	return mapping
 }
