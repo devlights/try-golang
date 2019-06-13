be52aa06953c65de3ec68f12fbf07fe96b8ee637
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-06-13 16:07:56 +0900

    Update

--- basic/iota_/iota01.go
+++ basic/iota_/iota01.go
@@ -3,13 +3,13 @@ package iota_
 
 import "fmt"
 
-type ByteSize int64
-
 const (
 	FirstValue int = iota // 最初の値は 0 からスタート
 	SecondValue
 	ThirdValue
 )
+
+type ByteSize int64
 
 const (
 	_           = iota // 最初の iota の値は捨てる
@@ -19,8 +19,18 @@ const (
 	TB
 )
 
+type Weekday int
+
+const (
+	Sunday  Weekday = iota + 1 // iota は 0
+	_                          // iota が加算
+	Monday                     // iota は 2, (2 + 1) で 3 となる
+	Tuesday                    // iota は 3, (3 + 1) で 4 となる
+)
+
 func Iota01() error {
 	fmt.Println(FirstValue, SecondValue, ThirdValue)
 	fmt.Println(KB, MB, GB)
+	fmt.Println(Sunday, Monday, Tuesday)
 	return nil
 }
