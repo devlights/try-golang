dbb8ead39d1f404a30c3e3fbe785e8ed15cdb320
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-09-04 18:49:54 +0900

    Update

--- basic/tutorial/tutorial_gotour.go
+++ basic/tutorial/tutorial_gotour.go
@@ -77,6 +77,23 @@ func GoTour() error {
 	//noinspection GoBoolExpressions
 	fmt.Println(boolVal, maxInt, maxUInt, byteVal, runeVal)
 
+	// ------------------------------------------------------------
+	// 初期値
+	// 変数に初期値を与えずに宣言すると、「ゼロ値」が設定される
+	// ゼロ値は型によって異なる
+	//
+	// - 数値型は 0
+	// - bool型は false
+	// - string型は ""
+	// ------------------------------------------------------------
+	var (
+		zeroInt    int
+		zeroBool   bool
+		zeroString string
+	)
+
+	fmt.Printf("%v\t%v\t%q\n", zeroInt, zeroBool, zeroString)
+
 	return nil
 }
 
