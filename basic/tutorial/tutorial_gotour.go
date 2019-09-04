d6c41b51541e93b58dd6676ccb90554e779c31b3
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-09-04 17:39:47 +0900

    Update

--- basic/tutorial/tutorial_gotour.go
+++ basic/tutorial/tutorial_gotour.go
@@ -46,6 +46,37 @@ func GoTour() error {
 	fmt.Println(multiParamMultiReturn(100, 200))
 	fmt.Println(multiParamMultiReturnWithReturnNames(100, 200))
 
+	var (
+		x, y = 100, 200
+	)
+
+	swap(&x, &y)
+	fmt.Println(x, y)
+
+	// ------------------------------------------------------------
+	// Go言語の基本型は以下
+	//
+	// - bool
+	// - string
+	// - int     (int8,   int16,  int32,  int64)
+	// - uint    (uint8 , uint16, uint32, uint64, uintptr)
+	// - byte    (uint8 の 別名)
+	// - rune    (int32 の 別名)
+	// - float   (float32, float64)
+	// - complex (complex32, complex64)
+	// ------------------------------------------------------------
+	//noinspection GoVarAndConstTypeMayBeOmitted
+	var (
+		boolVal bool   = true
+		maxInt  int64  = 1<<63 - 1
+		maxUInt uint64 = 1<<64 - 1
+		byteVal byte   = 42
+		runeVal rune   = 'a'
+	)
+
+	//noinspection GoBoolExpressions
+	fmt.Println(boolVal, maxInt, maxUInt, byteVal, runeVal)
+
 	return nil
 }
 
@@ -77,4 +108,9 @@ func multiParamMultiReturnWithReturnNames(x, y int) (rx, ry, rsum int) {
 	rsum = x + y
 
 	return
+}
+
+// 指定された 2つの値 を入れ替え
+func swap(x, y *int) {
+	*x, *y = *y, *x
 }
