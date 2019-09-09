93187ce7d4ed45b918b73429022b674356abf19f
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-09-09 19:31:53 +0900

    add io.Reader examples

--- lib/mapping.go
+++ lib/mapping.go
@@ -126,4 +126,5 @@ func (m SampleMapping) MakeMapping() {
 	m["tutorial_gotour_type_switch"] = tutorial.GoTourTypeSwitch
 	m["tutorial_gotour_stringer"] = tutorial.GoTourStringer
 	m["tutorial_gotour_error"] = tutorial.GoTourError
+	m["tutorial_gotour_reader"] = tutorial.GoTourReader
 }
