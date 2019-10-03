5424781dabe5e009dcd8a6f6560b8eaa6d0a05fa
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-10-03 11:50:22 +0900

    add effective-go formatting

---
+++ effectivego/formatting.go
@@ -0,0 +1,20 @@
+package effectivego
+
+// Effective Go - Formatting の 内容についてのサンプルです。
+func Formatting() error {
+	/*
+		https://golang.org/doc/effective_go.html#formatting
+
+		- Go ではソースコードのフォーマットの問題に対してツールで対応する方針を持っている
+			- go fmt
+			- gofmt
+		- go fmt がパッケージ向け。 go fmt は内部で gofmt を呼び出している。
+		- Go ではインデントにタブを使う
+		- Go では一行の文字数を規定していない。 PythonのPEP8とかだと規定がある。
+		- なので、 Go で作業している際はコメントの位置合わせなどを気にする必要はない
+		- ソースコードを書いたら go fmt するようにする
+			- IDE (Golandなど) や VSCode などを利用していると自動でやってくれる
+	*/
+
+	return nil
+}
