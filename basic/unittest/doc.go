c80ebe10d4941aa2d343b80196b0bd04f4e4c50d
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-10-08 19:20:57 +0900

    add unit test example

---
+++ basic/unittest/doc.go
@@ -0,0 +1,23 @@
+/*
+Go言語の ユニットテスト についてのサンプルが配置されているパッケージです。
+
+このパッケージのみ、バイナリ実行でサンプルを動かすことができません。
+実行する場合は
+
+$ cd basic/unittest
+
+$ go test -v ./...
+
+とするか
+
+$ go test -v github.com/devlights/try-golang/basic/unittest
+
+としてください。
+
+参考
+
+https://medium.com/better-programming/easy-guide-to-unit-testing-in-golang-4fc1e9d96679
+
+https://github.com/golang/go/wiki/LearnTesting
+*/
+package unittest
