6c11304155df5aac1464c922e040c578ee9b4d43
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-10-29 19:47:17 +0900

    update gotour example

--- tutorial/tutorial_gotour_06_zerovalue.go
+++ tutorial/tutorial_gotour_06_zerovalue.go
@@ -1,6 +1,9 @@
 package tutorial
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // ZeroValue は、 Tour of Go - Zero values (https://tour.golang.org/basics/12) の サンプルです。
 func ZeroValue() error {
@@ -12,14 +15,35 @@ func ZeroValue() error {
 	// - 数値型は 0
 	// - bool型は false
 	// - string型は ""
+	// - 配列は長さ0の配列
+	// - スライスは長さ0のスライス
+	// - マップはキーの数が0のマップ
 	// ------------------------------------------------------------
 	var (
 		zeroInt    int
 		zeroBool   bool
 		zeroString string
+		zeroArray  [0]int
+		zeroSlice  []int
+		zeroMap    map[int]int
 	)
 
-	fmt.Printf("%v\t%v\t%q\n", zeroInt, zeroBool, zeroString)
+	_print(zeroInt, zeroBool, zeroString, zeroArray, zeroSlice, zeroMap)
+	_print(len(zeroArray), len(zeroSlice), len(zeroMap))
 
 	return nil
+}
+
+func _print(items ...interface{}) {
+	formats := make([]string, len(items), len(items))
+	for i := 0; i < len(formats); i++ {
+		formats[i] = "%#v"
+	}
+
+	format := strings.Join(formats, "\t")
+	if len(format) > 0 {
+		format = fmt.Sprintf("%v\n", format)
+	}
+
+	fmt.Printf(format, items...)
 }
