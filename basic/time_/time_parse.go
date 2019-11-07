1dfd357d8e71dbb34794ff2c87f416d044edd5a1
devlights <gsf.zero1@devlights.onmicrosoft.com>
2019-11-07 19:14:47 +0900

    update time.Parse() example.

--- basic/time_/time_parse.go
+++ basic/time_/time_parse.go
@@ -13,6 +13,25 @@ func TimeParse() error {
 	// ref: https://golang.org/pkg/time/#Parse
 	//
 	// layout に指定した書式で、valueに指定された文字列を日付として解析する。
+	// Goの日付書式は変わっていて、 yyyyとかの指定ではなく以下のように決まっている。
+	//
+	// 年： 2006
+	// 月： 01
+	// 日： 02
+	// 時：　15
+	// 分:  04
+	// 秒:  05
+	//
+	// タイムゾーンは、 Z07:00 と指定する。
+	//
+	// つまり、日本でよく使う yyyy/MM/dd HH:mm:ss は以下のようにする。
+	//
+	//     2006/01/02 15:04:05
+	//
+	// タイムゾーンも付ける場合は、こうなる
+	//
+	//     2006/01/02 15:04:05Z07:00
+	//
 	// ------------------------------------------------------------
 	// 正しい書式を与えると time.Time が取得できる
 	d1, err := time.Parse(time.RFC3339, "2019-11-07T10:11:12+09:00")
@@ -20,9 +39,11 @@ func TimeParse() error {
 		return err
 	}
 
-	d2 := d1.UTC()
+	fmt.Println("d1", d1)
 
-	fmt.Printf("d1: %s\nd2: %s\n", d1, d2)
+	// 上で取得した d1 は、ローカルタイムなのでUTCにするには以下のようにする
+	d2 := d1.UTC()
+	fmt.Println("d2", d2)
 
 	// 不正な値を指定するとエラーとなる
 	d3, err := time.Parse(time.RFC3339, "not a date")
@@ -30,8 +51,45 @@ func TimeParse() error {
 		fmt.Println(err)
 	} else {
 		// ここに来ることは無いが、d3の値は 「0001-01-01T00:00:00Z」 となる
-		fmt.Println(d3)
+		fmt.Println("d3", d3)
 	}
+
+	// 日本でよく使われる yyyy/MM/dd HH:mm:ss の場合はこのようになる
+	const jpnFormat = "2006/01/02 15:04:05"
+	d4, err := time.Parse(jpnFormat, "2019/11/07 10:11:12")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("d4", d4)
+
+	// 上記でParseした d4 は、タイムゾーンを付与していないので UTC となる
+	// JSTの状態でParseしたい場合は以下のようにする
+	//
+	// time.Local は、 ローカルタイムゾーンを表す
+	// time.UTC は、 UTCタイムゾーンを表す
+	d5, err := time.ParseInLocation(jpnFormat, "2019/11/07 10:11:12", time.Local)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("d5", d5)
+
+	// または、フォーマットの方にタイムゾーンを付与して、Parseしても良い
+	const jpnFormatWithTimeZone = "2006/01/02 15:04:05Z07:00"
+	d6, err := time.Parse(jpnFormatWithTimeZone, "2019/11/07 10:11:12+09:00")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("d6", d6)
+
+	// 無理やりだが、 UTC時間を9時間減算してタイムゾーン情報を当てても同じ結果となる
+	fmt.Println(d4.Add(-9 * time.Hour).In(time.Local))
+
+	// ローカルタイムゾーンに関しては、以下でも取得できる
+	loc, _ := time.LoadLocation("Asia/Tokyo")
+	fmt.Println(d4.Add(-9 * time.Hour).In(loc))
 
 	return nil
 }
