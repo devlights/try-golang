48865dc61a94b22e9050a7ed326a12e2ee8c4391
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-03-09 20:03:21 +0900

    Add with_pipeline.go

--- examples/advanced/errgrp/examples.go
+++ examples/advanced/errgrp/examples.go
@@ -2,6 +2,7 @@ package errgrp
 
 import (
 	"github.com/devlights/try-golang/examples/advanced/errgrp/cmpwaitgroup"
+	"github.com/devlights/try-golang/examples/advanced/errgrp/pipeline"
 	"github.com/devlights/try-golang/examples/advanced/errgrp/withcontext"
 	"github.com/devlights/try-golang/mappings"
 )
@@ -18,4 +19,5 @@ func (r *register) Regist(m mappings.ExampleMapping) {
 	m["errgrp_error_with_waitgroup"] = cmpwaitgroup.ErrWithWaitGroup
 	m["errgrp_error_with_errgroup"] = cmpwaitgroup.ErrWithErrGroup
 	m["errgrp_with_context"] = withcontext.ErrGroupWithContext
+	m["errgrp_with_pipeline"] = pipeline.ErrGroupWithPipeline
 }
