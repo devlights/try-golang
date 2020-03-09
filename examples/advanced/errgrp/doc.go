4a6a9a48f4a7ab7c8f73d8381beae7191a43080c
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-03-09 16:57:34 +0900

    Add error_with_waitgroup.go

---
+++ examples/advanced/errgrp/doc.go
@@ -0,0 +1,13 @@
+/*
+Package errgrp は、 sync.ErrGroup のサンプルが配置されています。
+
+
+REFERENCES::
+
+  - https://godoc.org/golang.org/x/sync/errgroup
+
+  - https://pkg.go.dev/golang.org/x/sync/errgroup?tab=doc
+
+  - https://deeeet.com/writing/2016/10/12/errgroup/
+*/
+package errgrp
