bc081cbea70709de68925a12a626bfc8786e9bb6
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-03-30 13:12:15 +0900

    Update effectivego_28_parallelization.go

--- effectivego/effectivego28/effectivego_28_parallelization.go
+++ effectivego/effectivego28/effectivego_28_parallelization.go
@@ -2,6 +2,8 @@ package effectivego28
 
 import (
 	"fmt"
+	"math/rand"
+	"runtime"
 	"sync"
 	"time"
 
@@ -60,7 +62,7 @@ func (c *chprc) calc(ch chan<- struct{}) {
 		ch <- struct{}{}
 	}(ch)
 
-	output.Stdoutf("[処理中]", "%v\n", c.name)
+	output.Stdoutf("[処理中]", "%v\ttime: %v\n", c.name, c.t1)
 	<-time.After(c.t1)
 	output.Stdoutf("[処理完]", "%v\n", c.name)
 }
@@ -72,7 +74,7 @@ func (w *wgprc) calc(wg *sync.WaitGroup) {
 		wg.Done()
 	}(wg)
 
-	output.Stdoutf("[処理中]", "%v\n", w.name)
+	output.Stdoutf("[処理中]", "%v\ttime: %v\n", w.name, w.t1)
 	<-time.After(w.t1)
 	output.Stdoutf("[処理完]", "%v\n", w.name)
 }
@@ -85,26 +87,27 @@ func Parallelization() error {
 		- 計算を独立して実行できる部分に分割することができれば並列化することが可能となる (全ての場合ではない)
 		  - 後、必要なのは各部分が完了したときに信号を送ること。これはチャネルを利用することで可能である。
 		  - それぞれの並行処理に対して sync.WaitGroup を渡しても良い。大事なのは全ての結果を待機することが可能な状態を作ること
+		- 並列処理を考慮する場合、マシンに搭載されているCPUの数より多く処理を発行したところで待ちが発生するだけなので並列数には気を使う必要がある
+		  - runtime.NumCPU() で CPU の数を取得できる
+		  - runtime.GOMAXPROCS(0) で 現在の最大同時実行可能ゴルーチンの数が取得できる
 	*/
 	var (
-		t1 = []time.Duration{
-			1 * time.Second,
-			2 * time.Second,
-			3 * time.Second,
-		}
+		numGoroutine = runtime.GOMAXPROCS(0)
 	)
-
 	var (
 		wg = sync.WaitGroup{}
-		ch = make(chan struct{}, len(t1))
+		ch = make(chan struct{}, numGoroutine)
 	)
 
-	for i := 0; i < len(t1); i++ {
-		proc := newChProc(fmt.Sprintf("chproc-%02d", i), t1[i])
+	defer close(ch)
+
+	for i := 0; i < numGoroutine; i++ {
+		td := time.Duration(rand.Intn(1000)) * time.Millisecond
+		proc := newChProc(fmt.Sprintf("chproc-%02d", i), td)
 		go proc.calc(ch)
 	}
 
-	for i := 0; i < len(t1); i++ {
+	for i := 0; i < numGoroutine; i++ {
 		// 完了するたびにゴルーチン側から値が送信されてくるので受信するようにする
 		// 起動したゴルーチンと同じ数の受信が行えたら、全非同期処理が完了したことになる
 		<-ch
@@ -113,8 +116,9 @@ func Parallelization() error {
 	output.Stdoutl("[ChProc]", "All Done")
 	output.StdoutHr()
 
-	for i := 0; i < len(t1); i++ {
-		proc := newWgProc(fmt.Sprintf("wgproc-%02d", i), t1[i])
+	for i := 0; i < numGoroutine; i++ {
+		td := time.Duration(rand.Intn(1000)) * time.Millisecond
+		proc := newWgProc(fmt.Sprintf("wgproc-%02d", i), td)
 		wg.Add(1)
 
 		go proc.calc(&wg)
