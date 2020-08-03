e2f96289d6565f7cf36b68e4422e60a08a69e37b
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-08-03 17:11:34 +0900

    Update racecondition.go

--- books/concurrency/chapter01/racecondition.go
+++ books/concurrency/chapter01/racecondition.go
@@ -21,8 +21,17 @@ func RaceCondition() error {
 	// (0 かもしれないし 1 かもしれない)
 	//
 	// どの順番で処理が走るのかは保証されていない.
+	//
+	// 今回のような非同期処理を書いた場合、変数 data は競合状態が発生するので
+	// 以下のような結果となる可能性がある
+	//
+	// - ゴルーチンが開始される前に if に到達してしまい、 data は 0
+	// - ゴルーチンが終了した後に if に到達してしまい、 data は 1
+	// - if が評価された時点では data は 0 だが、結果を出力する前にゴルーチンが動き data は 1
 	if data == 0 {
 		output.Stdoutl("[result]", "data is zero")
+	} else {
+		output.Stdoutl("[reslt]", "data is non-zero")
 	}
 
 	return nil
