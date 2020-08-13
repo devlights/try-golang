c25ba2aa06a4a85c903704c9941506f9c1023cd8
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-08-14 02:45:25 +0900

    Add example (#244)
    close #244

--- examples/basic/variables/examples.go
+++ examples/basic/variables/examples.go
@@ -25,4 +25,5 @@ func (r *register) Regist(m mappings.ExampleMapping) {
 	m["short_assignment_statement"] = shortassignment.Basic
 	m["shadowing_variable"] = shadowing.Basic
 	m["using_ref_to_loop_iterator_variable"] = loopiterator.CommonMistakePattern
+	m["passing_loop_variable_to_goroutine_by_pointer"] = loopiterator.PassingLoopVariableToGoroutineByPointer
 }
---
+++ examples/basic/variables/loopiterator/passing_a_loop_variable_to_goroutine_by_pointer.go
@@ -0,0 +1,90 @@
+package loopiterator
+
+import (
+	"sync"
+
+	"github.com/devlights/gomy/output"
+)
+
+type (
+	value struct {
+		i int
+	}
+)
+
+// PassingLoopVariableToGoroutineByPointer -- ループ変数をポインタ経由でGoroutineに渡した場合のサンプルです.
+//
+// REFERENCES:
+//   - https://stackoverflow.com/a/23637430
+//   - https://golang.org/doc/effective_go.html#channels
+func PassingLoopVariableToGoroutineByPointer() error {
+	// ----------------------------------------------------------------
+	// Go では、ループ変数は使いまわしされるので
+	// 例えば、ループ内でGoroutineを起動する際にループ変数を
+	// ポインタで渡すような書き方をすると、実際には同じアドレスを
+	// 渡していることになる。
+	//
+	// なので、各Goroutineは、ほぼ同じ値を見ることになってしまう。
+	// (Goroutineがとても早く起動して、変化する前の値をみた場合はちゃんとした値が見える)
+	//
+	// 回避策としては
+	// 渡す前にコピーをとって渡すようにするか、添字を使って
+	// 値を取得するようにすれば大丈夫
+	// ----------------------------------------------------------------
+	badpattern()
+
+	output.StdoutHr()
+
+	goodpattern()
+
+	return nil
+}
+
+func badpattern() {
+	var (
+		wg   = &sync.WaitGroup{}
+		vals = []value{
+			{1},
+			{2},
+			{3},
+		}
+	)
+
+	wg.Add(len(vals))
+	for _, v := range vals {
+		output.Stdoutf("[bad][v]", "addr=%p, value=%v\n", &v, v)
+
+		go func(v *value) {
+			defer wg.Done()
+			output.Stdoutf("[bad][v][goroutine]", "addr=%p, value=%v\n", v, *v)
+		}(&v)
+	}
+
+	wg.Wait()
+}
+
+func goodpattern() {
+	var (
+		wg   = &sync.WaitGroup{}
+		vals = []value{
+			{1},
+			{2},
+			{3},
+		}
+	)
+
+	wg.Add(len(vals))
+	for _, v := range vals {
+		output.Stdoutf("[good][v]", "addr=%p, value=%v\n", &v, v)
+
+		// 渡す前にコピーを取得
+		//   - https://stackoverflow.com/a/23637430
+		v := v
+		go func(v *value) {
+			defer wg.Done()
+			output.Stdoutf("[good][v][goroutine]", "addr=%p, value=%v\n", v, *v)
+		}(&v)
+	}
+
+	wg.Wait()
+}
