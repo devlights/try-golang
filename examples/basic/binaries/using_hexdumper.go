25cc452ccc277e5eaa29946a16765c42f962aed5
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-08-14 16:51:18 +0900

    Update example

--- examples/basic/binaries/using_hexdumper.go
+++ examples/basic/binaries/using_hexdumper.go
@@ -16,6 +16,9 @@ func UsingHexDumper() error {
 	// 実行した場合のような16進数ダンプを出力することができる.
 	//
 	// hex.Dumper は、io.Writer を受け取り、io.WriteCloserを返す.
+	//
+	// 最後に Close を呼ばないと、ダンプ出力の右側に元の値が表示されないので注意
+	// (Close を呼ばないままだと、16進部分のみが出力される)
 	// -----------------------------------------------------
 	var (
 		s      string         = "hello world"
@@ -23,6 +26,11 @@ func UsingHexDumper() error {
 		w      io.Writer      = os.Stdout
 		dumper io.WriteCloser = hex.Dumper(w)
 	)
+
+	defer func() {
+		// Close を呼ぶことにより、出力の右側に値が出力される
+		_ = dumper.Close()
+	}()
 
 	output.Stdoutl("[original]", s)
 
