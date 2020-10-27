c04b00320a383b58c26ca1d9af016deb23240543
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-10-27 17:01:34 +0900

    Add handling -vscode option

--- cmd/trygolang/main.go
+++ cmd/trygolang/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/devlights/try-golang/builder"
 	"github.com/devlights/try-golang/mappings"
@@ -17,6 +19,21 @@ func main() {
 
 	args = NewArgs()
 	args.Parse()
+
+	if args.RunWithVsCode {
+		if _, err := os.Stat(".target"); os.IsNotExist(err) {
+			log.Println("VSCode 経由で実行する場合は .target ファイルが必要です")
+			log.Println("(.target ファイルの中に実行したいサンプル名を入れてください)")
+			log.Fatal("終了します...")
+		}
+
+		b, err := ioutil.ReadFile(".target")
+		if err != nil {
+			log.Fatalf("Cannot read .target file")
+		}
+
+		args.ExampleName = strings.TrimRight(string(b), "\n")
+	}
 
 	mapping = builder.BuildMappings()
 
