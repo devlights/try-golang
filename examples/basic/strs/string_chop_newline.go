506344f0c392fd4ca1f7428529ae3e3be5412131
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-10-29 13:29:00 +0900

    Update string_chop_newline.go

--- examples/basic/strs/string_chop_newline.go
+++ examples/basic/strs/string_chop_newline.go
@@ -9,23 +9,40 @@ import (
 // ChopNewLine -- 文字列末尾の改行を削除するサンプルです
 func ChopNewLine() error {
 	var (
-		withNewLine    = "helloworld\n"
+		withLF         = "helloworld\n"
+		withCRLF       = "helloworld\r\n"
 		withoutNewLine = "helloworld"
 	)
 
-	output.Stdoutl("[has newline? (withNewLine)      ]", strings.HasSuffix(withNewLine, "\n"))
+	output.Stdoutl("[has newline? (withLF)           ]", strings.HasSuffix(withLF, "\n"))
+	output.Stdoutl("[has newline? (withCRLF)         ]", strings.HasSuffix(withCRLF, "\n"))
 	output.Stdoutl("[has newline? (withoutNewLine)   ]", strings.HasSuffix(withoutNewLine, "\n"))
 
 	// 末尾の改行を削除
-	chopped := strings.TrimRight(withNewLine, "\n")
+	chopLF := chop(withLF)
 
-	output.Stdoutl("[has newline? (chopped)          ]", strings.HasSuffix(chopped, "\n"))
-	output.Stdoutl("[equal? (chopped==withoutNewLine)]", chopped == withoutNewLine)
+	output.Stdoutl("[has newline? (chopped)(LF)      ]", strings.HasSuffix(chopLF, "\n"))
+	output.Stdoutl("[equal? (chopped==withoutNewLine)]", chopLF == withoutNewLine)
+
+	chopCRLF := chop(withCRLF)
+	output.Stdoutl("[has newline? (chopped)(CRLF)    ]", strings.HasSuffix(chopCRLF, "\n"))
+	output.Stdoutl("[equal? (chopped==withoutNewLine)]", chopCRLF == withoutNewLine)
 
 	output.StdoutHr()
-	output.Stdoutl("[with   ]", []byte(withNewLine))
-	output.Stdoutl("[chopped]", []byte(chopped))
-	output.Stdoutl("[without]", []byte(withoutNewLine))
+	output.Stdoutl("[LF      ]", []byte(withLF))
+	output.Stdoutl("[CRLF    ]", []byte(withCRLF))
+	output.Stdoutl("[chopLF  ]", []byte(chopLF))
+	output.Stdoutl("[chopCRLF]", []byte(chopCRLF))
+	output.Stdoutl("[without ]", []byte(withoutNewLine))
 
 	return nil
+}
+
+func chop(s string) string {
+	s = strings.TrimRight(s, "\n")
+	if strings.HasSuffix(s, "\r") {
+		s = strings.TrimRight(s, "\r")
+	}
+
+	return s
 }
