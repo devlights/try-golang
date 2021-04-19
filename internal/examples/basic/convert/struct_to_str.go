7a37e253780c322f48a6febbda2ec1ec0e3210bc
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-04-19 14:05:01 +0000

    Update example

--- internal/examples/basic/convert/struct_to_str.go
+++ internal/examples/basic/convert/struct_to_str.go
@@ -6,25 +6,41 @@ import (
 	"github.com/devlights/gomy/output"
 )
 
-type sts struct {
-	id   int
-	name string
-}
+type (
+	sts struct {
+		id   int
+		name string
+	}
+
+	yen int
+)
 
 func (s sts) String() string {
 	return fmt.Sprintf("id=%d,name=%s", s.id, s.name)
 }
 
+func (y yen) String() string {
+	return fmt.Sprintf("¥%d", y)
+}
+
 // StructToStr は、fmt.Sprint() を利用して 構造体 を 文字列 にするサンプルです.
 func StructToStr() error {
+	var (
+		s1 = sts{
+			id:   100,
+			name: "sts",
+		}
+		y = yen(1000)
+	)
 
-	s1 := sts{
-		id:   100,
-		name: "sts",
-	}
+	var (
+		fn = func(o fmt.Stringer) {
+			output.Stdoutf("[struct to str]", "%[1]v(%[1]T) --> %[2]q(%[2]T)\n", o, fmt.Sprint(o))
+		}
+	)
 
-	s := fmt.Sprint(s1)
-	output.Stdoutf("[struct to str]", "%[1]v(%[1]T) --> %[2]q(%[2]T)\n", s1, s)
+	fn(s1)
+	fn(y)
 
 	return nil
 }
