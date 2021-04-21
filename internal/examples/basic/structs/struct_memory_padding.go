e9974f1e78bfdfc4e184f4b06068eb881a02bc81
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-04-21 09:27:00 +0000

    Update example

--- internal/examples/basic/structs/struct_memory_padding.go
+++ internal/examples/basic/structs/struct_memory_padding.go
@@ -10,13 +10,14 @@ import (
 // MemoryPadding は、構造体メンバーの定義順によってGoランタイムがメモリ上にパディングを挿入することを確認するサンプルです.
 func MemoryPadding() error {
 	var (
-		notGood = types.MemoryPadding{}
-		good = types.NoMemoryPadding{}
+		notGood = types.MemoryPadding{}   // メモリのパディングが発生する構造体
+		good    = types.NoMemoryPadding{} // メモリのパディングが発生しない構造体
 	)
 
-	output.Stdoutl("[Padding 発生]", unsafe.Sizeof(notGood))
+	output.Stdoutf("[Padding 発生]", "%d byte(s)\n", unsafe.Sizeof(notGood))
 	output.Stdoutl("", notGood.Layout())
-	output.Stdoutl("[Padding なし]", unsafe.Sizeof(good))
+	output.StdoutHr()
+	output.Stdoutf("[Padding なし]", "%d byte(s)\n", unsafe.Sizeof(good))
 	output.Stdoutl("", good.Layout())
 
 	return nil
