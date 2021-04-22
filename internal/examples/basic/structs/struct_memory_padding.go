88cb9124218c223bd45d0eb88e43ed27219b29f0
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-04-22 05:40:11 +0000

    Update example

--- internal/examples/basic/structs/struct_memory_padding.go
+++ internal/examples/basic/structs/struct_memory_padding.go
@@ -8,6 +8,11 @@ import (
 )
 
 // MemoryPadding は、構造体メンバーの定義順によってGoランタイムがメモリ上にパディングを挿入することを確認するサンプルです.
+//
+// REFERENCES:
+//   - https://itnext.io/structure-size-optimization-in-golang-alignment-padding-more-effective-memory-layout-linters-fffdcba27c61
+//   - https://logicalbeat.jp/blog/4032/
+//   - https://ja.wikipedia.org/wiki/%E3%83%87%E3%83%BC%E3%82%BF%E6%A7%8B%E9%80%A0%E3%82%A2%E3%83%A9%E3%82%A4%E3%83%A1%E3%83%B3%E3%83%88
 func MemoryPadding() error {
 	var (
 		st4bytes = types.Struct4Bytes{}    // メモリ上のサイズが 4bytes になる構造体
