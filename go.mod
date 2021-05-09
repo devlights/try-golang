443ac1e024aeacee4127980a3b786806ab81963e
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-05-09 10:53:13 +0000

    Move sentry-go examples to try-golang-extlib

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.16
 
 require (
 	github.com/adrg/xdg v0.3.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/devlights/gomy v0.3.0
-	github.com/getsentry/sentry-go v0.10.0
 	golang.org/x/crypto v0.0.0-20210218145215-b8e89b74b9df
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
 	golang.org/x/text v0.3.5
--- internal/examples/basic/logging/examples.go
+++ internal/examples/basic/logging/examples.go
@@ -17,9 +17,6 @@ func NewRegister() mappings.Register {
 func (r *register) Regist(m mappings.ExampleMapping) {
 	m["log_flags"] = Flags
 	m["log_prefix"] = Prefix
-	m["log_sentry_basic"] = SentryBasic
-	m["log_sentry_goroutine_bad"] = SentryGoroutineBad
-	m["log_sentry_goroutine_good"] = SentryGoroutineGood
 	m["log_output"] = Output
 	m["log_new"] = NewLogger
 }
