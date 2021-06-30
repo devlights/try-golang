163035ccb85186d85ba7eef4dcd85e36084a110c
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-06-30 05:14:59 +0000

    Add go test -short のサンプル

---
+++ cmd/testing_short_feature/lib/lib.go
@@ -0,0 +1,17 @@
+package lib
+
+func Add(x, y int) int {
+	return x+y
+}
+
+func Sum(num ...int8) int {
+	var (
+		total int
+	)
+
+	for _, x := range num {
+		total += int(x)
+	}
+
+	return total
+}
---
+++ cmd/testing_short_feature/main.go
@@ -0,0 +1,33 @@
+/*
+	go test の -short オプションを付与した際のサンプルです。
+	詳細は lib/lib_test.go を参照ください。
+
+	REFERENCES:
+	  - https://dev.to/jonasbn/til-skipping-tests-in-go-3i5l
+	  - https://golang.org/cmd/go/#hdr-Testing_flags
+	  - https://golang.org/pkg/testing/#hdr-Skipping
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var (
+		exitCode int
+		err      error
+	)
+
+	if err = run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		exitCode = 1
+	}
+
+	os.Exit(exitCode)
+}
+
+func run() error {
+	return nil
+}
