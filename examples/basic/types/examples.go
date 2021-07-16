1a87f9affa3480e5807a35fa72579fe9267668ef
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-07-16 00:47:41 +0000

    Add types/define_types_easy.go

--- examples/basic/types/examples.go
+++ examples/basic/types/examples.go
@@ -13,5 +13,6 @@ func NewRegister() mapping.Register {
 
 // Regist -- 登録します.
 func (r *register) Regist(m mapping.ExampleMapping) {
-	m["type_basic"] = Basic
+	m["types_basic"] = Basic
+	m["types_define_types_easy"] = DefineTypesEasy
 }
