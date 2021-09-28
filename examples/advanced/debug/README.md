5bddc6a8081127e533c5c0efae631a315593440c
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-09-28 03:40:27 +0000

    依存しているライブラリの情報をランタイム時に取得するサンプル Fixes #434

---
+++ examples/advanced/debug/README.md
@@ -0,0 +1,7 @@
+# サンプルリスト
+
+このディレクトリには以下のサンプルがあります。
+
+|file|example name|note|
+|----|------------|----|
+|build\_info.go|debug\_build\_info|debug.ReadBuildInfo を使ったサンプルです|
--- examples/advanced/examples.go
+++ examples/advanced/examples.go
@@ -4,6 +4,7 @@ import (
 	"github.com/devlights/try-golang/examples/advanced/async"
 	"github.com/devlights/try-golang/examples/advanced/closure"
 	"github.com/devlights/try-golang/examples/advanced/crypto"
+	"github.com/devlights/try-golang/examples/advanced/debug"
 	"github.com/devlights/try-golang/examples/advanced/deepcopy"
 	"github.com/devlights/try-golang/examples/advanced/errgrp"
 	"github.com/devlights/try-golang/examples/advanced/imaging"
@@ -26,6 +27,7 @@ func (r *register) Regist(m mapping.ExampleMapping) {
 	async.NewRegister().Regist(m)
 	closure.NewRegister().Regist(m)
 	crypto.NewRegister().Regist(m)
+	debug.NewRegister().Regist(m)
 	deepcopy.NewRegister().Regist(m)
 	errgrp.NewRegister().Regist(m)
 	imaging.NewRegister().Regist(m)
