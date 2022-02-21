28f58d2884141640eb459e300594ae6820471cf0
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-02-21 05:13:59 +0000

    Update example

--- examples/basic/stacktrace/debug_printstack.go
+++ examples/basic/stacktrace/debug_printstack.go
@@ -9,40 +9,45 @@ import (
 )
 
 // DebugPrintStack -- runtime/debug.PrintStack のサンプルです.
+//
+// REFERENCES
+//   - https://pkg.go.dev/runtime/debug#PrintStack
+//   - https://stackoverflow.com/questions/19094099/how-to-dump-goroutine-stacktraces
 func DebugPrintStack() error {
+	var (
+		third = func(pCtx context.Context) context.Context {
+			ctx, cxl := context.WithTimeout(pCtx, 100*time.Millisecond)
+			defer cxl()
+
+			// この処理が走っているgoroutineのスタックトレースを出力 (全部ではない)
+			output.StderrHr()
+			debug.PrintStack()
+
+			<-ctx.Done()
+
+			return ctx
+		}
+		second = func(pCtx context.Context) context.Context {
+			ctx, cxl := context.WithCancel(pCtx)
+			go func() {
+				defer cxl()
+				<-third(ctx).Done()
+			}()
+
+			return ctx
+		}
+		first = func(pCtx context.Context) context.Context {
+			ctx, cxl := context.WithCancel(pCtx)
+			go func() {
+				defer cxl()
+				<-second(ctx).Done()
+			}()
+
+			return ctx
+		}
+	)
+
 	<-first(context.Background()).Done()
+
 	return nil
-}
-
-func first(pCtx context.Context) context.Context {
-	ctx, cxl := context.WithCancel(pCtx)
-	go func() {
-		defer cxl()
-		<-second(ctx).Done()
-	}()
-
-	return ctx
-}
-
-func second(pCtx context.Context) context.Context {
-	ctx, cxl := context.WithCancel(pCtx)
-	go func() {
-		defer cxl()
-		<-third(ctx).Done()
-	}()
-
-	return ctx
-}
-
-func third(pCtx context.Context) context.Context {
-	ctx, cxl := context.WithTimeout(pCtx, 100*time.Millisecond)
-	defer cxl()
-
-	// この処理が走っているgoroutineのスタックトレースを出力 (全部ではない)
-	output.StderrHr()
-	debug.PrintStack()
-
-	<-ctx.Done()
-
-	return ctx
 }
