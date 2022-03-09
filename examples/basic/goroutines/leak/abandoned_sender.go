bfb797580f39ac5fa1cc1f782f45db486005c070
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-03-09 02:16:17 +0000

    Add examples/basic/goroutines/leak pkg

---
+++ examples/basic/goroutines/leak/abandoned_sender.go
@@ -0,0 +1,48 @@
+package leak
+
+import (
+	"context"
+	"time"
+
+	"github.com/devlights/gomy/output"
+)
+
+// AbandonedSender -- goroutineリークが発生するパターンのサンプルです。
+//
+// チャネルの送受信の実装があるが、タイミングによっては受信側がいなくなってしまうパターン。
+// 送信側のgoroutineが永遠に待ち続けるので終了しません。
+//
+// 解決方法としては、Bufferedなチャネルを使うこと。
+//
+// REFERENCES:
+//   - https://betterprogramming.pub/common-goroutine-leaks-that-you-should-avoid-fe12d12d6ee
+func AbandonedSender() error {
+	var (
+		ctx, cxl = context.WithTimeout(context.Background(), 10*time.Millisecond)
+		ch       = make(chan int)
+		iowait   = func() {
+			time.Sleep(1 * time.Second)
+		}
+		fn = func(ch chan<- int) {
+			iowait()
+			ch <- 1
+			output.Stdoutl("[send]", 1)
+		}
+	)
+	defer cxl()
+
+	go fn(ch)
+	select {
+	case v := <-ch:
+		output.Stdoutl("[recv]", v)
+	case <-ctx.Done():
+	}
+
+	//
+	// チャネルからデータを受信するものがいなくなるので
+	// 上のgoroutineはプロセスが起動中は永遠に終了しません。
+	//
+	time.Sleep(1 * time.Second)
+
+	return nil
+}
---
+++ examples/basic/goroutines/leak/forgotten_sender.go
@@ -0,0 +1,36 @@
+package leak
+
+import (
+	"time"
+
+	"github.com/devlights/gomy/output"
+)
+
+// ForgottenSender -- goroutineリークが発生するパターンのサンプルです。
+//
+// チャネルを作成し、チャネルの送信側がいないパターン。
+// 受信側のgoroutineが永遠に待ち続けるので終了しません。
+//
+// 解決方法としては、送信側が適切に使い終わったチャネルを閉じること。
+//
+// REFERENCES:
+//   - https://betterprogramming.pub/common-goroutine-leaks-that-you-should-avoid-fe12d12d6ee
+func ForgottenSender() error {
+	var (
+		fn = func(ch <-chan int) {
+			data := <-ch
+			output.Stdoutl("[goroutine leak]", data)
+		}
+		ch = make(chan int)
+	)
+
+	go fn(ch)
+
+	//
+	// チャネルにデータを送信するものがいないので
+	// 上のgoroutineはプロセスが起動中は永遠に終了しません。
+	//
+	time.Sleep(1 * time.Second)
+
+	return nil
+}
