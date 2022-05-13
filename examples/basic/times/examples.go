0d7f5e62f5f626a48d502d4a650f67ac641d4c87
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-05-13 07:29:03 +0000

    Add time_json_custom.go

--- examples/basic/times/examples.go
+++ examples/basic/times/examples.go
@@ -25,5 +25,6 @@ func (r *register) Regist(m mapping.ExampleMapping) {
 	m["time_changelocaltimezone"] = ChangeLocalTimezone
 	m["time_truncate"] = Truncate
 	m["time_json"] = TimeJson
+	m["time_json_custom"] = TimeJsonCustom
 	m["time_in"] = TimeIn
 }
