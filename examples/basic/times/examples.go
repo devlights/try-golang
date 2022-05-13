fb96c4a791c417931f5f29749a8e3cc60cc176c4
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-05-13 06:22:04 +0000

    Add time_json.go

--- examples/basic/times/examples.go
+++ examples/basic/times/examples.go
@@ -24,4 +24,5 @@ func (r *register) Regist(m mapping.ExampleMapping) {
 	m["time_afterfunc"] = AfterFunc
 	m["time_changelocaltimezone"] = ChangeLocalTimezone
 	m["time_truncate"] = Truncate
+	m["time_json"] = TimeJson
 }
