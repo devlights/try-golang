23e5e7b9ce43a7295d4ffc2f2dd47d338c838644
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-05-17 06:49:24 +0000

    Add socket tcp basic example

---
+++ examples/singleapp/socket/tcp_basic/client/main.go
@@ -0,0 +1,82 @@
+// Go でのソケットプログラミング 基本 (1)
+//
+// 本パッケージはクライアント側の処理です。
+//
+// REFERENCES
+//   - https://www.developer.com/languages/intro-socket-programming-go/
+//   - https://stackoverflow.com/questions/13417095/how-do-i-stop-a-listening-server-in-go
+//   - https://stackoverflow.com/a/237495
+//
+
+package main
+
+import (
+	"errors"
+	"io"
+	"log"
+	"net"
+	"os"
+
+	"github.com/devlights/gomy/errs"
+)
+
+var (
+	appLog = log.New(os.Stdout, "[client]", 0)
+	errLog = log.New(os.Stderr, "[client]", 0)
+)
+
+func main() {
+	//
+	// 接続
+	//
+	var (
+		getAddr = net.ResolveTCPAddr
+		laddr   = errs.Forgot(getAddr("tcp", "localhost:"))
+		raddr   = errs.Forgot(getAddr("tcp", "localhost:8888"))
+		conn    *net.TCPConn
+		err     error
+	)
+
+	conn, err = net.DialTCP("tcp", laddr, raddr)
+	if err != nil {
+		errLog.Fatal(err)
+	}
+	defer conn.Close()
+
+	appLog.Printf("connected to: %s\n", conn.RemoteAddr())
+
+	//
+	// 送信
+	//
+	var (
+		message = []byte("hello world")
+	)
+
+	_, err = conn.Write(message)
+	if err != nil {
+		appLog.Printf("error at conn.Write (%v)", err)
+		return
+	}
+
+	//
+	// 受信
+	//
+	var (
+		buf       = make([]byte, 4096)
+		bytesRead int
+	)
+
+	bytesRead, err = conn.Read(buf)
+	switch {
+	case err != nil && !errors.Is(err, io.EOF):
+		appLog.Printf("error at conn.Read (%v)", err)
+		return
+	case bytesRead == 0:
+		appLog.Printf("closed by remote")
+		return
+	default:
+		appLog.Printf("%d bytes recv", bytesRead)
+	}
+
+	appLog.Printf("recv: %s\n", buf[:bytesRead])
+}
