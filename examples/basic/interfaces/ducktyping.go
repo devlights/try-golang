a2eb1dc3fac18e637521458d200d620df415dac2
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-06-15 23:44:20 +0000

    Add duck-typing example

---
+++ examples/basic/interfaces/ducktyping.go
@@ -0,0 +1,31 @@
+package interfaces
+
+import (
+	"fmt"
+
+	"github.com/devlights/gomy/output"
+)
+
+type myType string
+
+func (me myType) Say() string {
+	return fmt.Sprintf("%T", me)
+}
+
+// DuckTyping2 -- ダックタイピングのちょっとしたサンプルです。
+//
+// REFERENCES:
+//   - https://thomasnguyen.hashnode.dev/duck-typing-in-go
+func DuckTyping2() error {
+	var (
+		fn = func(obj interface{ Say() string }) {
+			output.Stdoutl("[fn]", obj.Say())
+		}
+	)
+
+	// If it looks like a duck, and it quacks like a duck, then it is a duck.
+	// アヒルのように見えて、アヒルのように鳴いていたら、それはアヒルである.
+	fn(myType(""))
+
+	return nil
+}
