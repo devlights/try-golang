0ab4727aa674a56b6a93ca002cae0645071aefed
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-08-08 04:33:59 +0000

    Add osop/listprocesses.go

--- examples/basic/osop/examples.go
+++ examples/basic/osop/examples.go
@@ -14,4 +14,5 @@ func NewRegister() mapping.Register {
 // Regist -- 登録します.
 func (r *register) Regist(m mapping.ExampleMapping) {
 	m["osop_mkdir"] = Mkdir
+	m["osop_list_processes"] = ListProcesses
 }
