0061a80b91e18c7aa390cf4cc1a8e1354905d983
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-08-24 07:33:39 +0000

    Add fmt.Append/Appendf/Appendln examples

---
+++ examples/basic/formatting/appendf.go
@@ -0,0 +1,24 @@
+package formatting
+
+import (
+	"fmt"
+
+	"github.com/devlights/gomy/output"
+)
+
+// AppendF -- Go 1.19 から追加された fmt.Appendf() のサンプルです。
+//
+// # REFERENCES
+//   - https://pkg.go.dev/fmt@go1.19#Appendf
+//   - https://dev.to/emreodabas/quick-guide-go-119-features-1j40
+func AppendF() error {
+	var (
+		buf = make([]byte, 0)
+		sli = []any{"hello", "world"}
+	)
+
+	buf = fmt.Appendf(buf, "%s %s", sli[1], sli[0])
+	output.Stdoutl("[fmt.Appendf]", string(buf))
+
+	return nil
+}
