425d3cf45c865b62e1ef3468e9c651abffc96c76
devlights <gsf.zero1@devlights.onmicrosoft.com>
2023-01-22 16:34:48 +0000

    Update

--- examples/socket/tcp_03_dial_with_context/main.go
+++ examples/socket/tcp_03_dial_with_context/main.go
@@ -24,6 +24,7 @@ func main() {
 	defer listener.Close()
 
 	// タイムアウトさせるために、わざと詰まらせる
+	// (バックログをフルにする)
 	net.Dial(proto, addr)
 	net.Dial(proto, addr)
 
@@ -63,10 +64,20 @@ func main() {
 
 // listen は、指定された情報で net.Listener を生成して返します.
 //
+// Go の net.Listen は、バックログが指定できないため
+// テスト目的でバックログを極端に少ない状態にすることが出来ない。
+// 利用される値は linux の場合は net.core.somaxconn の値であり
+// 存在しない場合は、 syscall.SOMAXCONN の定数値が利用される。
+//
+// なので、バックログを指定したい場合はソケットを直接生成して
+// net.FileListener に紐づけて、リスナーとして利用する。
+//
 // # REFERENCES
+//   - https://github.com/golang/go/issues/39000
 //   - https://github.com/golang/go/issues/41470
 //   - https://github.com/valyala/tcplisten/blob/master/tcplisten.go
 //   - https://stackoverflow.com/a/49593356
+//   - https://meetup-jp.toast.com/1509
 func listen(addr string, backLog int) (net.Listener, error) {
 	// make tcp addr
 	var (
