606be282236c7f3f78b845e284ca0170989f21e5
devlights <gsf.zero1@devlights.onmicrosoft.com>
2024-03-11 04:24:36 +0000

    Add examples/basic/strs/trimspace.go

---
+++ examples/basic/strs/trimspace.go
@@ -0,0 +1,29 @@
+package strs
+
+import (
+	"strings"
+
+	"github.com/devlights/gomy/output"
+)
+
+// TrimSpace は、strings.TrimSpace() のサンプルです.
+//
+// 両端のスペースをトリミングしてくれる。
+//
+// > TrimSpace returns a slice of the string s, with all leading and trailing white space removed, as defined by Unicode.
+//
+// > (TrimSpaceは、Unicodeで定義されているように、すべての先頭と末尾の空白を除去した文字列sのスライスを返します。)
+//
+// # REFERENCES
+//   - https://pkg.go.dev/strings@go1.22.1#TrimSpace
+func TrimSpace() error {
+	var (
+		withSpace = "   hello world    "
+		noSpace   = "hello world"
+	)
+
+	output.Stdoutf("[withSpace]", "%q\n", strings.TrimSpace(withSpace))
+	output.Stdoutf("[noSpace  ]", "%q\n", strings.TrimSpace(noSpace))
+
+	return nil
+}
