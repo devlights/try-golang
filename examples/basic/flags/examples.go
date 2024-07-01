c508c04696f0184453e99920b7dfb6adb6d7c022
devlights <gsf.zero1@devlights.onmicrosoft.com>
2024-07-01 05:58:59 +0000

    Add examples/basic/flags/flagset.go

---
+++ examples/basic/flags/examples.go
@@ -0,0 +1,17 @@
+package flags
+
+import "github.com/devlights/try-golang/mapping"
+
+type (
+	register struct{}
+)
+
+// NewRegister -- このパッケージ用のサンプルを登録する mapping.Register を生成します。
+func NewRegister() mapping.Register {
+	return &register{}
+}
+
+// Regist -- 登録します.
+func (r *register) Regist(m mapping.ExampleMapping) {
+	m["flags_flagset"] = Flagset
+}
