30f29beaf3a737e676da938294d5d9fb4964321a
devlights <gsf.zero1@devlights.onmicrosoft.com>
2024-10-16 05:10:10 +0000

    Update

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-go install github.com/go-task/task/v3/cmd/task@latest
-go install honnef.co/go/tools/cmd/staticcheck@latest
-go install golang.org/x/tools/cmd/goimports@latest
-go install github.com/go-delve/delve/cmd/dlv@latest
+#go install github.com/go-task/task/v3/cmd/task@latest
+#go install honnef.co/go/tools/cmd/staticcheck@latest
+#go install golang.org/x/tools/cmd/goimports@latest
+#go install github.com/go-delve/delve/cmd/dlv@latest
