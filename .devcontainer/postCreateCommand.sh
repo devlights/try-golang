bdf7a5f3b0a9a5229c530f4a519c2af8db5da86f
devlights <gsf.zero1@devlights.onmicrosoft.com>
2024-10-16 07:40:38 +0000

    Update

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,5 @@ go install github.com/go-task/task/v3/cmd/task@latest
 go install honnef.co/go/tools/cmd/staticcheck@latest
 go install golang.org/x/tools/cmd/goimports@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
+
+sudo apt update && sudo apt install -y universal-ctags
