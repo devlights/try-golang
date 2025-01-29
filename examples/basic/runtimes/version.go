47c2abb7849cdb2b8c7e8d674e5a2b2e1b3b17f4
devlights <gsf.zero1@devlights.onmicrosoft.com>
2025-01-29 04:46:18 +0000

    Update basic/runtimes/version.go

--- examples/basic/runtimes/version.go
+++ examples/basic/runtimes/version.go
@@ -3,6 +3,7 @@ package runtimes
 import (
 	"fmt"
 	"runtime"
+	"runtime/debug"
 )
 
 // RuntimeVersion は、runtime.Version() のサンプルです。
@@ -11,20 +12,28 @@ func RuntimeVersion() error {
 	ver := runtime.Version()
 	fmt.Println(ver)
 
+	// debug.ReadBuildInfo() からも取得することが出来る
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return fmt.Errorf("failed: debug.ReadBuildInfo()")
+	}
+	fmt.Println(info.GoVersion)
+
 	return nil
 
 	/*
-	   $ task
-	   task: Task "build" is up to date
-	   task: [run] ./try-golang -onetime
+		$ task
+		task: [build] go build -o "/workspace/try-golang/try-golang" .
+		task: [run] ./try-golang -onetime
 
-	   ENTER EXAMPLE NAME: runtime_version
+		ENTER EXAMPLE NAME: runtime_version
 
-	   [Name] "runtime_version"
-	   go1.21.6
+		[Name] "runtime_version"
+		go1.23.5
+		go1.23.5
 
 
-	   [Elapsed] 3.21µs
+		[Elapsed] 48.21µs
 	*/
 
 }
