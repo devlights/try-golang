3e7a1f67b1c2c27d3fd3be4d46f3e3e7cf71d368
devlights <gsf.zero1@devlights.onmicrosoft.com>
2025-02-16 14:32:22 +0000

    Add byteop/go124_bytes_lines.go

--- examples/basic/byteop/examples.go
+++ examples/basic/byteop/examples.go
@@ -16,4 +16,5 @@ func (r *register) Regist(m mapping.ExampleMapping) {
 	m["byteop_reader_from_byteslice"] = ReaderFromByteSlice
 	m["byteop_cut_prefix_suffix"] = CutPrefixSuffix
 	m["byteop_using_repeat"] = UsingRepeat
+	m["byteop_go124_bytes_lines"] = Go124BytesLines
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/devlights/try-golang
 
-go 1.23
+go 1.24
 
 require (
 	github.com/devlights/gomy v0.6.0
