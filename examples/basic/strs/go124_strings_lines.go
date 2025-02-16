78aae4aed20601ef5dc077f2731e42a43805f0c5
devlights <gsf.zero1@devlights.onmicrosoft.com>
2025-02-16 14:44:32 +0000

    Add strs/go124_strings_lines.go

--- examples/basic/strs/go124_strings_lines.go
+++ examples/basic/strs/go124_strings_lines.go
@@ -1,28 +1,29 @@
-package byteop
+package strs
 
 import (
-	"bytes"
 	"fmt"
 	"iter"
 	"os"
+	"strings"
+	"unsafe"
 )
 
-// Go124BytesLines は、Go 1.24 で追加された bytes.Lines() のサンプルです.
+// Go124StringsLines は、Go 1.24 で追加された strings.Lines() のサンプルです.
 //
 // Go 1.23 で追加されたイテレータを返すようになっています。
-// bytes.Lines()は、バイト列を '\n' で区切ったデータをイテレータで提供してくれます。
+// strings.Lines()は、文字列を '\n' で区切ったデータをイテレータで提供してくれます。
 // このデータには '\n' 自身も付いた状態で取得出来るので、その点には注意が必要。
 // (fmt.Println()でそのまま出力すると２個改行が入ることになる)
 //
 // # REFERENCES
-//   - https://pkg.go.dev/bytes@go1.24.0#Lines
-//   - https://tip.golang.org/doc/go1.24#bytespkgbytes
+//   - https://pkg.go.dev/strings@go1.24.0#Lines
+//   - https://tip.golang.org/doc/go1.24#stringspkgstrings
 //   - https://tip.golang.org/doc/go1.23#iterators
-func Go124BytesLines() error {
+func Go124StringsLines() error {
 	var (
 		// 最初のN行のみに絞るためのイテレータ関数
-		firstN = func(n int, lines iter.Seq[[]byte]) iter.Seq[[]byte] {
-			return func(yield func([]byte) bool) {
+		firstN = func(n int, lines iter.Seq[string]) iter.Seq[string] {
+			return func(yield func(string) bool) {
 				count := 0
 				for line := range lines {
 					if count >= n || !yield(line) {
@@ -34,7 +35,7 @@ func Go124BytesLines() error {
 			}
 		}
 		// 前に番号を付与するイテレータ
-		withLineNum = func(lines iter.Seq[[]byte]) iter.Seq[string] {
+		withLineNum = func(lines iter.Seq[string]) iter.Seq[string] {
 			return func(yield func(string) bool) {
 				count := 1
 				for line := range lines {
@@ -46,11 +47,15 @@ func Go124BytesLines() error {
 				}
 			}
 		}
+		// []byteからstringへ変換するためのヘルパー関数
+		toStr = func(b []byte) string {
+			return unsafe.String(unsafe.SliceData(b), len(b))
+		}
 
-		data, _ = os.ReadFile("README.md") // ファイルデータを読み出して
-		lines   = bytes.Lines(data)        // 行単位にして
-		first5  = firstN(5, lines)         // 先頭５行のみにして
-		withNum = withLineNum(first5)      // 行番号を付与
+		data, _ = os.ReadFile("README.md")   // ファイルデータを読み出して
+		lines   = strings.Lines(toStr(data)) // 行単位にして
+		first5  = firstN(5, lines)           // 先頭５行のみにして
+		withNum = withLineNum(first5)        // 行番号を付与
 	)
 
 	for line := range withNum {
@@ -64,9 +69,9 @@ func Go124BytesLines() error {
 	   task: [build] go build -o "/workspace/try-golang/try-golang" .
 	   task: [run] ./try-golang -onetime
 
-	   ENTER EXAMPLE NAME: byteop_go124_bytes_lines
+	   ENTER EXAMPLE NAME: string_go124_strings_lines
 
-	   [Name] "byteop_go124_bytes_lines"
+	   [Name] "string_go124_strings_lines"
 
 	   	1:
 	   	2: # try-golang
@@ -74,6 +79,6 @@ func Go124BytesLines() error {
 	   	4: This is my TUTORIAL project for golang.
 	   	5:
 
-	   [Elapsed] 127.78µs
+	   [Elapsed] 94.12µs
 	*/
 }
