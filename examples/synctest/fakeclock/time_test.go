24dd37ed846e7511956848d8248d18eaf4cee2f1
devlights <gsf.zero1@devlights.onmicrosoft.com>
2025-09-03 14:11:07 +0900

    Update

--- examples/synctest/fakeclock/time_test.go
+++ examples/synctest/fakeclock/time_test.go
@@ -1,4 +1,4 @@
-package faketime
+package fakeclock
 
 import (
 	"testing"
@@ -12,7 +12,7 @@ const (
 	SleepTime = 2 * time.Second
 )
 
-func TestNoFakeTime(t *testing.T) {
+func TestNoFakeClock(t *testing.T) {
 	start := time.Now()
 	{
 		// 普通に時間が流れるので指定時間スリープする
@@ -23,15 +23,15 @@ func TestNoFakeTime(t *testing.T) {
 	be.Equal(t, elapsed, SleepTime) // 実際の結果は実行時に変化し、基本的に厳密な一致は出来ないことがほとんど
 }
 
-func TestUseFakeTime(t *testing.T) {
+func TestUseFakeClock(t *testing.T) {
 	synctest.Test(t, func(t *testing.T) {
 		start := time.Now()
 		{
-			// Fake-Timeが使用されるためsynctest内では一瞬で完了する
+			// Fake-Clockが使用されるためsynctest内では一瞬で完了する
 			time.Sleep(SleepTime)
 		}
 		elapsed := time.Since(start)
 
-		be.Equal(t, elapsed, SleepTime) // Fake-Timeにより結果が一致する
+		be.Equal(t, elapsed, SleepTime) // Fake-Clockにより結果が一致する
 	})
 }
