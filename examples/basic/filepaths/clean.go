3abcfe9c7d4c2e079c3da9c0de43e253fcb84b44
devlights <gsf.zero1@devlights.onmicrosoft.com>
2026-06-15 10:30:12 +0900

    Update example

--- examples/basic/filepaths/clean.go
+++ examples/basic/filepaths/clean.go
@@ -21,24 +21,30 @@ import (
 //
 //  4. 絶対パスの先頭の /.. を / に変える（ルートより上には行かせない）
 //
+//  5. 空文字列の置換 (パスが空文字の場合はカレントディレクトリに変換)
+//
 // # REFERENCES
 //   - https://pkg.go.dev/path/filepath@go1.26.3#Clean
 func Clean() error {
-	//  1. 連続するセパレータを 1 個にする
+	// 1. 連続するセパレータを 1 個にする
 	v := filepath.Clean("foo/bar//baz//////file.txt")
 	output.Stdoutl("[No.1]", v)
 
-	//  2. カレント(".")を消す
+	// 2. カレント(".")を消す
 	v = filepath.Clean("./dokokano/dir/./file.txt")
 	output.Stdoutl("[No.2]", v)
 
-	//  3. パスの途中の .. を解決して、直前の要素とセットで消す
+	// 3. パスの途中の .. を解決して、直前の要素とセットで消す
 	v = filepath.Clean("./dokokano/dir/../file.txt")
 	output.Stdoutl("[No.3]", v)
 
-	//  4. 絶対パスの先頭の /.. を / に変える（ルートより上には行かせない）
+	// 4. 絶対パスの先頭の /.. を / に変える（ルートより上には行かせない）
 	v = filepath.Clean("/../dokokano/dir/file.txt")
 	output.Stdoutl("[No.4]", v)
+
+	// 5. 空文字列の置換 (パスが空文字の場合はカレントディレクトリに変換)
+	v = filepath.Clean("")
+	output.Stdoutl("[No.5]", v)
 
 	return nil
 }
